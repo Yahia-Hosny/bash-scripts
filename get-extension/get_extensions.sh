5011528c142985208310e99d5991662474a45178
Yahia Hosny <yahiahosny71@gmail.com>
2024-01-09 05:04:37 +0200

    finish the basic version

--- get-extension/get_extensions.sh
+++ get-extension/get_extensions.sh
@@ -12,6 +12,16 @@
 #''
 
 
+read -p "what is your first name : " name 
+echo " "
+read -p "what is your surname/family name  : " surname
+echo " "
+read -N 4 -p "what is your extension number   : " exnum
+echo " "
+read -s -N 4 -p  "What access code would you like to use when dialing in: " code
+echo " "
+echo "$name,$surname,$exnum,$code" >> extensions.csv​
+
  
 
 
