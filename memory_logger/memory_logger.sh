0ec2c050830dbe76e13b694723c67ebd0bf36372
Yahia Hosny <yahiahosny71@gmail.com>
2024-01-20 03:01:17 +0200

    funish the first version

--- memory_logger/memory_logger.sh
+++ memory_logger/memory_logger.sh
@@ -6,13 +6,17 @@
 #Last Modified : 20/01/2024
 
 #Description
-#'' 
+#'Append the output of the free command to ​ $HOME/performance/memory.log' 
 
 #Usage
 #''
-
-
+directory=$HOME/performance
+if [ -d "$directory" ] ;then 
+    echo "$directory" is exist 
+else
+    mkdir -p "$directory"
+fi
+free >> "$directory"/memory.log
  
-
 
 exit 0
