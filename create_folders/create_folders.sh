69ecd37ee86003e303a2bbf5a2a427f38f564a28
Yahia Hosny <yahiahosny71@gmail.com>
2024-01-20 07:05:24 +0200

    add create folders script

---
+++ create_folders/create_folders.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+# Author : YAHIA HOSNY 
+# Date Created :20/01/2024 
+#Last Modified :20/01/2024
+
+#Description
+#'create a folder structure for sharing network ' 
+
+#Usage
+#''
+
+a="new_direc"
+while read line ; do
+
+mkdir -p "$a"/"$line"
+    
+done < "$1" 
+
+echo "the procces's done "
+ 
+
+
+exit 0
